a799489ef1105cf265f1a70f177244bfcb021eb6
David <david.holstein01@gmail.com>
2024-12-10 15:04:35 +0100

    Initial commit

---
+++ simple_inquiry.py
@@ -0,0 +1,28 @@
+import bluetooth
+import subprocess
+
+class CInquiry(object):
+
+    def __init__(self):
+        pass
+
+    def SearchDevices():
+        nearby_devices = bluetooth.discover_devices(lookup_names=True)
+        print("Found {} devices.".format(len(nearby_devices)))
+
+        for addr, name in nearby_devices:
+            print("  {} - {}".format(addr, name))
+
+    def StartBluetoothService():
+        try:
+            subprocess.run(["sudo","systemctl","enable","bluetooth"], check = True)
+            print("Blootooth aktiviert")
+
+        except subprocess.CalledProcessError as e:
+            print(f"fehler: {e}")
+
+
+            
+
+if __name__ == '__main__':
+    CInquiry.StartBluetoothService()
