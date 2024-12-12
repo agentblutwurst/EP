d9b15e36109dbca6611fe53f809fc265195d8e73
David <david.holstein01@gmail.com>
2024-12-12 12:29:49 +0100

    tzzhrftjhrft

--- simple_inquiry.py
+++ simple_inquiry.py
@@ -58,16 +58,53 @@ class CInquiry(object):
 
         except subprocess.CalledProcessError as e:
             print(f"Fehler: {e}")
+
+    """def CheckBluetoothConnection():
+        try:
+            result = subprocess.run(["hciconfig"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+            if "UP RUNNING\n" in result.stdout:
+                print(result.stdout, " , passt")
+
+            else:
+                print(result.stdout, " , funzt net")
+
+        except subprocess.CalledProcessError as e:
+            print(f"Fehler: {e}")"""
+
+    def CheckIfBluetooth():
+        try:
+            subprocess.run(["bluetoothctl"], input="show".encode(), text=False)
+
+            print(" , passt")
+
+        except subprocess.CalledProcessError as e:
+            print(f"Fehler: {e}")
+
+    def CheckIfBluetooth0():
+        try:
+            result = subprocess.run(["bluetoothctl"], input="power on\nagent on".encode(), text=False, stdout=subprocess.PIPE)
+            word_to_count = "[bluetooth]"
+            word_count = str(result.stdout).count(word_to_count)
+
+            print(result.stdout)
+            print(word_count)
+
+        except subprocess.CalledProcessError as e:
+            print(f"Fehler: {e}")
+
             
 
 if __name__ == '__main__':
     st = time.time()
 
     #CInquiry.StartBluetoothService()
-    CInquiry.UnblockWirelessConnections()
-    CInquiry.EnableBluetoothService()
-    CInquiry.StartBluetoothService()
-    CInquiry.MakeDiscoverable()
+    #CInquiry.UnblockWirelessConnections()
+    #CInquiry.EnableBluetoothService()
+    #CInquiry.StartBluetoothService()
+    #CInquiry.MakeDiscoverable()
+    #CInquiry.CheckIfBluetooth()
+    CInquiry.CheckIfBluetooth0()
     
 
     et = time.time()
