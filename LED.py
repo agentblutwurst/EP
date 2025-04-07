60911a30fcce29cbde6b8e37f4b746199aa7a4a3
EP-Lautsprecher <david.holstein01@gmail.com>
2025-04-07 10:21:55 +0200

    LED.py erfolgreich getestet

--- LED.py
+++ LED.py
@@ -15,49 +15,65 @@ LED_INVERT = 0              # Set to 1 to invert the LED signal, good if using N
 DOT_COLORS = [0x200000,     # red
               0x002000,     # green
               0x000020,     # blue
-              0x20000000,   # white
-              0]            # off
+              0x101010,     # white
+              0x0]            # off
 
-FrequencyBand = [2, 3, 4, 1, 5, 8, 2, 10, 3, 7]
+FrequencyBand1 = [2, 3, 4, 1, 5, 8, 2, 10, 3, 7]
+FrequencyBand2 = [4, 1, 5, 8, 2, 10, 3, 7, 2, 3]
+FrequencyBand3 = [5, 8, 2, 10, 3, 7, 2, 3, 4, 1]
+
+FrequencyBands = [FrequencyBand1,
+                  FrequencyBand2,
+                  FrequencyBand3]
 
 # Initialisiere LED-Controller (Low-Level)
 leds = ws.new_ws2811_t()
 
+for channum in range(2):
+    channel = ws.ws2811_channel_get(leds, channum)
+    ws.ws2811_channel_t_count_set(channel, 0)
+    ws.ws2811_channel_t_gpionum_set(channel, 0)
+    ws.ws2811_channel_t_invert_set(channel, 0)
+    ws.ws2811_channel_t_brightness_set(channel, 0)
+
 # Konfiguriere Kanal (hier nur Kanal 0 als Beispiel)
-channel = ws.ws2811_channel_get(leds, 0)
+channel = ws.ws2811_channel_get(leds, LED_CHANNEL)
+
 ws.ws2811_channel_t_count_set(channel, LED_COUNT)
 ws.ws2811_channel_t_gpionum_set(channel, LED_GPIO)
 ws.ws2811_channel_t_brightness_set(channel, LED_BRIGHTNESS)
 ws.ws2811_channel_t_invert_set(channel, LED_INVERT)
 
+ws.ws2811_t_freq_set(leds, LED_FREQ_HZ)
+ws.ws2811_t_dmanum_set(leds, LED_DMA_NUM)
+
 # Initialisierung der LED-Engine
 ws.ws2811_init(leds)
-
-# Fester Farbwert für Rot (0x200000)
-RED_COLOR = 0x200000
 
 # Animation
 try:
     while True:
         # Iteriere durch alle Frequenzbänder
-        for i in range(10):  # 10 Frequenzbänder
-            # Berechne die Start- und Endposition für das Frequenzband im LED-Streifen
-            start_led = i * 10  # Beginn der Säule (jede Säule hat 10 LEDs)
-            end_led = start_led + FrequencyBand[i]  # Endposition der Säule (basierend auf dem Wert)
+        for h in range(len(FrequencyBands)):
+            for i in range(10):  # 10 Frequenzbänder
+                # Berechne die Start- und Endposition für das Frequenzband im LED-Streifen
+                start_led = i * 10  # Beginn der Säule (jede Säule hat 10 LEDs)
+                end_led = start_led + FrequencyBands[h][i]  # Endposition der Säule (basierend auf dem Wert)
 
-            # Setze LEDs für dieses Frequenzband (Säule)
-            for j in range(10):  # Jede Säule hat 10 LEDs
-                led_index = start_led + j  # Berechne den LED-Index im Gesamtstreifen
-                if led_index < end_led:  # LEDs bis zum Wert des Frequenzbandes leuchten lassen
-                    ws.ws2811_led_set(channel, led_index, DOT_COLORS[0])
-                else:
-                    ws.ws2811_led_set(channel, led_index, DOT_COLORS[4])  # Restliche LEDs ausschalten
+                # Setze LEDs für dieses Frequenzband (Säule)
+                for j in range(10):  # Jede Säule hat 10 LEDs
+                    led_index = start_led + j  # Berechne den LED-Index im Gesamtstreifen
+                    if led_index < end_led:  # LEDs bis zum Wert des Frequenzbandes leuchten lassen
+                        ws.ws2811_led_set(channel, led_index, DOT_COLORS[h])
+                    else:
+                        ws.ws2811_led_set(channel, led_index, DOT_COLORS[4])  # Restliche LEDs ausschalten
 
-        # Rendere die neuen Werte auf die LEDs
-        ws.ws2811_render(leds)
+            # Rendere die neuen Werte auf die LEDs
+            ws.ws2811_render(leds)
 
-        # Zeitintervall von 42,6 ms
-        time.sleep(0.0426)
+            # Zeitintervall von 42,6 ms
+            time.sleep(1)
+            # time.sleep(0.0426)
 finally:
     # Aufräumen bei Programmende
     ws.ws2811_fini(leds)
