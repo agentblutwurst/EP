7f73e191aaffab16d758bc175a7e5d37e69f0b4a
EP-Lautsprecher <david.holstein01@gmail.com>
2025-05-13 14:43:54 +0200

    Taster im test.py

--- Test.py
+++ Test.py
@@ -1,3 +1,16 @@
-import subprocess
+import RPi.GPIO as GPIO
+import time
 
-subprocess.run(["sudo", "python", "/home/EP/Documents/Projekt/LED_test.py"])
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def KnoppGedrueckt(PinNummer):    
+    Zustand = GPIO.input(PinNummer) == GPIO.LOW
+    return Zustand
+
+while True:
+    print(KnoppGedrueckt(17))
+    print(KnoppGedrueckt(22))
+    time.sleep(0.25)
